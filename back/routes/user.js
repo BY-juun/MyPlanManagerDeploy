402b0518817f9f85ac67c80046d0eafac63a1076
ByeongJun <neostgeart@ajou.ac.kr>
2021-08-29 05:16:25 +0900

    bcryptjs

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { User } = require('../models'); 
 const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 const passport = require('passport');
